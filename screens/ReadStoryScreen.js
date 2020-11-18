aff64a201a47a7eb5c49d4c942d62cf5cd6a40f2
samuel112233 <samuelbhowmik@gmail.com>
2020-11-18 12:44:16 +0530

    Bedtime Stories - Search Functionality

--- screens/ReadStoryScreen.js
+++ screens/ReadStoryScreen.js
@@ -1,11 +1,53 @@
- import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import { StyleSheet, Text, View, FlatList } from 'react-native';
+import db from '../config';
 
 export default class ReadStoryScreen extends React.Component {
+  constructor(){
+    super()
+    this.state={
+      allStories:[]
+    }
+  }
+  componentDidMount(){
+    this.getStories()
+  }
+
+
+  getStories=()=>{
+    try{
+      var allStories = []
+      db.collection('newstories').get()
+      .then(querySnapshot=>{
+        querySnapshot.forEach(doc=>{
+          allStories.push(doc.data())
+          console.log(allStories)
+        })
+        this.setState({
+          allStories: allStories
+        })
+      })
+    }
+    catch{
+      console.log(error)
+    }
+  }
     render(){
         return(
-            <View style={styles.container}>
-                <Text>Read Story</Text>
+            <View >
+                <FlatList 
+                data = {this.state.allStories}
+                renderItem={({item})=>(
+                  <View style = {styles.itemContainer}>
+                    <Text>TITLE: {item.title}</Text>
+                    <Text>Author: {item.author}</Text>
+                    </View>
+                )}
+                keyExtractor = {(item,index)=>index.toString()}
+
+                
+                />
+
             </View>
         );
     }
@@ -18,4 +60,11 @@ const styles = StyleSheet.create({
     alignItems: 'center',
     justifyContent: 'center',
   },
+  itemContainer:{
+    height:80,
+    width:'100%',
+    borderWidth:2,
+    justifyContent:'center',
+    alignItems:'center'
+  }
 });
